296521a66047e5a809cb4e91178844198f8c46b1
Your Name <bigtulahi1@gmaiil.com>
2023-08-03 03:40:48 +0100

    webkit and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
+"use client";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 500); // delay time in milliseconds
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className=" text-center">Hello</h1>
+      <div
+        className={`transform transition-all duration-500 ${
+          isVisible ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
+        }`}
+      >
+        This div will slide in from the bottom and fade in when the page loads.
+      </div>
       <Link href={"/play-game"} as={"/play-game"}>
         play game
       </Link>
--- src/components/InputDropdown.tsx
+++ src/components/InputDropdown.tsx
@@ -85,7 +85,7 @@ const InputDropdown: React.FC<InputDropdownProps> = ({ options, callback }) => {
       <input
         type="search"
         id="default-search"
-        className="block w-full px-3 py-2 text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-transparent"
+        className="block appearance-searchfield w-full px-3 py-2 text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-transparent"
         placeholder="Guess the manhwa"
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
